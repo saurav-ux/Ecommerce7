133f53d429ac7a0b3c79c8a72220e8c83e34ec06
Saurav Anand <saurav.anand@auctopustechnologies.com>
2023-09-23 00:25:31 +0530

    redux

--- src/Services/containerSlice.js
+++ src/Services/containerSlice.js
@@ -1,17 +1,25 @@
 import { createSlice} from "@reduxjs/toolkit";
 
 const initialState={
-    count:0
+    count:0,
+    addproduct: []
 }
 const containerSlice = createSlice({
     name:'cake',
     initialState,
     reducers:{
         totalCost:(state,action)=>{
-            state.count=state.count+action.payload;
+            state.count=state.count+action.payload.price;
+        },
+        addItem:(state,action)=>{
+            return{
+                ...state,
+                addproduct: [...state.addproduct,action.payload],
+            }
         }
+        
     }
 })
 
 export default containerSlice.reducer
-export const {totalCost}= containerSlice.actions
+export const {totalCost,addItem}= containerSlice.actions
